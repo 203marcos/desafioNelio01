c1cf93f54b9435a559872e02a407baf22e60719d
Marcos Dias <marcosaurelioce@hotmail.com>
2025-12-12 01:50:37 -0300

    feat: using autowired in the services

--- src/main/java/com/desafioNelio/desafio/services/OrderService.java
+++ src/main/java/com/desafioNelio/desafio/services/OrderService.java
@@ -1,16 +1,13 @@
 package com.desafioNelio.desafio.services;
 
 import com.desafioNelio.desafio.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-
-    private final ShippingService shippingService;
-
-    public OrderService(ShippingService shippingService) {
-        this.shippingService = shippingService;
-    }
+    @Autowired
+    private ShippingService shippingService;
 
     public double total(Order order) {
         double basic = order.getBasic();
